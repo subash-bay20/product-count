7ce967ee4d2f66560a2b88ac98fb408e1c3cd261
subash-bay20 <subash.bay20@gmail.com>
2025-09-20 12:51:05 +0545

    Intial commit for count

---
+++ main.js
@@ -0,0 +1,19 @@
+const plus = document.getElementById('plus');
+const minus = document.getElementById('minus')
+const display = document.getElementById('display');
+
+
+plus.addEventListener('click', function add() {
+  if (display.value < 10) {
+    display.value++
+  }
+})
+
+minus.addEventListener('click', function sub() {
+  if (display.value <= 10 && display.value > 1) {
+    display.value--
+  }
+  else {
+    display.value = 1;
+  }
+})
